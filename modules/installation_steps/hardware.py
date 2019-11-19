f9b8e378d1e6856d1cd1d9482b205b18ec594474
Anton Hvornum <anton.feeds+github@gmail.com>
2019-11-19 01:05:35 +0000

    Removed debugging

--- modules/installation_steps/hardware.py
+++ modules/installation_steps/hardware.py
@@ -137,16 +137,16 @@ class parser():
 				if not storage['SAFETY_LOCK']:
 					archinstall.cache_diskpw_on_disk()
 					archinstall.close_disks()
-					fmt = spawn(client, archinstall.format_disk, debug=True, drive='drive', start='start', end='size')
+					fmt = spawn(client, archinstall.format_disk, drive='drive', start='start', end='size')
 					refresh = spawn(client, archinstall.refresh_partition_list, drive='drive', dependency=fmt)
 					mkfs = spawn(client, archinstall.mkfs_fat32, drive='drive', partition='1', dependency=refresh)
 					encrypt = spawn(client, archinstall.encrypt_partition, drive='drive', partition='2', keyfile='pwfile', dependency=mkfs)
 					mount_luksdev = spawn(client, archinstall.mount_luktsdev, drive='drive', partition='2', keyfile='pwfile', dependency=encrypt)
 					btrfs = spawn(client, archinstall.mkfs_btrfs, dependency=mount_luksdev)
-					progress['formatting'] = spawn(client, archinstall.mount_mountpoints, debug=True, drive='drive', bootpartition='1', callback=notify_partitioning_done, dependency=btrfs)
-					progress['strap_in'] = spawn(client, archinstall.strap_in_base, debug=True, callback=notify_base_install_done, dependency=progress['formatting'])
-					progress['configure_base_system'] = spawn(client, archinstall.configure_base_system, debug=True, callback=notify_base_configuration, dependency=progress['strap_in'])
-					progress['setup_bootloader'] = spawn(client, archinstall.setup_bootloader, debug=True, callback=notify_bootloader_completion, dependency=progress['configure_base_system'])
+					progress['formatting'] = spawn(client, archinstall.mount_mountpoints, drive='drive', bootpartition='1', callback=notify_partitioning_done, dependency=btrfs)
+					progress['strap_in'] = spawn(client, archinstall.strap_in_base, callback=notify_base_install_done, dependency=progress['formatting'])
+					progress['configure_base_system'] = spawn(client, archinstall.configure_base_system, callback=notify_base_configuration, dependency=progress['strap_in'])
+					progress['setup_bootloader'] = spawn(client, archinstall.setup_bootloader, callback=notify_bootloader_completion, dependency=progress['configure_base_system'])
 					# TODO: Call add_AUR_support()
 
 				else:
