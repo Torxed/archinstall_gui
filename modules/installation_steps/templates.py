afe2750e6e634ee58f9e4e697d4c6aef2d8bc081
Anton Hvornum <anton.feeds+github@gmail.com>
2020-03-16 17:29:42 +0100

    Added progressbar to templates

--- modules/installation_steps/templates.py
+++ modules/installation_steps/templates.py
@@ -88,6 +88,22 @@ def request_input(key, *args, **kwargs):
 		return storage[key]
 	return None
 
+last_update = time.time() # We generally don't need this since we're pushing through localhost. But just to not spam he UI.
+def progressbar(worker, output, *args, **kwargs):
+	global last_update
+	if len(output.strip()) and time.time() - last_update > 0.5:
+		try:
+			output = output.decode('UTF-8').strip()
+			sockets[worker.client.sock.fileno()].send({
+				'type' : 'notification',
+				'source' : 'base_os',
+				'message' : str(output[:120]),
+				'status' : 'active'
+			})
+			last_update = time.time()
+		except:
+			pass
+
 class parser():
 	def parse(path, client, data, headers, fileno, addr, *args, **kwargs):
 		log(f'templates got: {json.dumps(data)}', level=4, origin='templates')
@@ -126,7 +142,7 @@ class parser():
 				archinstall.instructions = archinstall.get_instructions(data['template'])
 				archinstall.instructions = archinstall.merge_in_includes(archinstall.instructions)
 				archinstall.cleanup_args(input_redirect=request_input)
-				progress['install_template'] = spawn(client, archinstall.run_post_install_steps, start_callback=notify_template_started, callback=notify_template_installed, dependency=progress['configure_base_system'])
+				progress['install_template'] = spawn(client, archinstall.run_post_install_steps, on_output=progressbar, start_callback=notify_template_started, callback=notify_template_installed, dependency=progress['configure_base_system'])
 
 				yield {
 						'status' : 'success',
