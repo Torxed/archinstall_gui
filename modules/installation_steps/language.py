2a0847959ae79a306d6b5019ee1c77e077d68a70
Anton Hvornum <anton.feeds+github@gmail.com>
2020-03-16 10:32:46 +0100

    Adding #7. Language now has a regional setting added, which will set the locale and run a locale-gen through archinstall.set_locale. Also updated archinstall to master to get this functionality.

--- modules/installation_steps/language.py
+++ modules/installation_steps/language.py
@@ -9,6 +9,15 @@ html = """
 	<h3>Regional / Language settings <i>(Optional)</i></h3>
 	<span>Some simpler language settings, such as timezone and locale.</span>
 
+	<div class="form-area" id="form-area">
+		<div class="input-form" id="input-form">
+			<input type="text" id="locale" required autocomplete="off" />
+			<label class="label">
+				<span class="label-content">Choose language & region <i>(Ex: 'en_SE.UTF-8 UTF-8' or 'SE' for short)</i>.</span>
+			</label>
+		</div>
+	</div>
+
 	<div class="buttons bottom">
 		<button id="save_language">Save settings</button>
 	</div>
@@ -22,11 +31,19 @@ javascript = """
 document.querySelector('#save_language').addEventListener('click', function() {
 	socket.send({
 		'_install_step' : 'language',
-		'language' : {}
+		'language' : document.querySelector('#locale').value
 	})
 })
 
 """
+
+def notify_language_set(worker, *args, **kwargs):
+	sockets[worker.client.sock.fileno()].send({
+		'type' : 'notification',
+		'source' : 'language',
+		'message' : 'Language has been configured.',
+		'status' : 'complete'
+	})
 
 class parser():
 	def parse(path, client, data, headers, fileno, addr, *args, **kwargs):
@@ -37,6 +54,9 @@ class parser():
 					'javascript' : javascript
 				}
 			else:
+
+				set_locale = spawn(client, archinstall.set_locale, fmt=data['language'], callback=notify_language_set)
+
 				yield {
 					'status' : 'success',
 					'next' : 'install_log'
